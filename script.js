35d99dcf4834a72ecfc266d656849f52de90423d
JosephThornton44 <143412779+JosephThornton44@users.noreply.github.com>
2023-09-25 05:52:11 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+fucntion  (bookAnswer)
+var passwrord =
+   document.getElementById("quizQuestion")   var passwordText=password.value.toUpperCase()
+   if(passwordText == "TELEPORT") {
+    return true;
+   }
+   alert("That is incorrect, try again!");
+   return false;
+   }
